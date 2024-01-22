761e34eb8379b4aa2083b86ff2fe82067f5aabd3
Nazar Kulish <nazarkuli66@gmail.com>
2024-01-22 08:02:02 +0200

    added each of final version

---
+++ eachOf/eachOf.js
@@ -0,0 +1,92 @@
+const {
+  toPromise,
+  wrapAsync,
+  executeFunctionOnlyOnce
+} = require('../util-async');
+
+const parallelEachOf = (coll, iteratee, callback) => {
+  if (callback) {
+    parallelEachOfLimitCallback(Infinity)(coll, iteratee, callback);
+  } else {
+    return toPromise(
+      parallelEachOfLimitCallback(Infinity),
+      coll,
+      iteratee,
+      callback
+    );
+  }
+};
+
+const seriesEachOf = (coll, iteratee, callback) => {
+  if (callback) {
+    parallelEachOfLimitCallback(1)(coll, iteratee, callback);
+  } else {
+    return toPromise(parallelEachOfLimitCallback(1), coll, iteratee, callback);
+  }
+};
+
+function parallelEachOfLimit(coll, limit, iteratee, callback) {
+  if (callback) {
+    parallelEachOfLimitCallback(limit)(coll, iteratee, callback);
+  } else {
+    return toPromise(
+      parallelEachOfLimitCallback(limit),
+      coll,
+      iteratee,
+      callback
+    );
+  }
+}
+
+function parallelEachOfLimitCallback(limit) {
+  return (coll, iteratee, callback) => {
+    if (limit <= 0) {
+      throw new RangeError('concurrency limit cannot be less than 1');
+    }
+    if (!coll || coll.length === 0) {
+      return callback(null);
+    }
+    let index = 0,
+      canceled = false,
+      running = 0,
+      looping = false,
+      done = false;
+    const _iteratee = wrapAsync(iteratee);
+    const _callback = executeFunctionOnlyOnce(callback);
+
+    function iterateeCallback(err, value) {
+      if (canceled) return;
+      running -= 1;
+      if (err) {
+        done = true;
+        canceled = true;
+        _callback(err);
+      } else if (done && running <= 0) {
+        done = true;
+        _callback(null);
+      } else if (!looping) {
+        processItems();
+      }
+    }
+    function processItems() {
+      looping = true;
+      while (running < limit && !done) {
+        var nextElem = coll[index];
+        if (nextElem === null || nextElem === undefined) {
+          done = true;
+          if (running <= 0) {
+            _callback(null);
+          }
+          return;
+        }
+        _iteratee(nextElem, index, executeFunctionOnlyOnce(iterateeCallback));
+        index += 1;
+        running += 1;
+      }
+      looping = false;
+    }
+    processItems();
+  };
+}
+
+module.exports = { parallelEachOf, parallelEachOfLimit, seriesEachOf };
